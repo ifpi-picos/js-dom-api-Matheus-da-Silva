c2359111e2c5180919a3073726490fae291762c2
Matheus da Silva <contagodzillajapan@gmail.com>
2024-04-03 00:57:39 -0300

    Definindo lista de tarefas e local storage

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ const dataInput = document.getElementById('data');
 const listaTarefas = document.getElementById('lista-tarefas');
 const cabecarioLista = document.getElementById('lista-tarefas-body');
 const nullMessage = document.getElementById('null');
+
+// Array para armazenar as tarefas
+let lista = [];
+
+// Carregar os tarefas do Local Storage ao carregar a página
+loadLocalStorage();
+
+function saveLocalStorage() {
+    localStorage.setItem('lista', JSON.stringify(lista));
+  }
+  
+  function loadLocalStorage() {
+    const savedlista = localStorage.getItem('lista');
+    if (savedlista) {
+      lista = JSON.parse(savedlista);
+      atualizarListaTarefas();
+    }
+}
