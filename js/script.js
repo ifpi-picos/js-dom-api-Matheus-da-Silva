ec15839243cbbce6af11ff7d94002a773cda1d18
Matheus da Silva <contagodzillajapan@gmail.com>
2024-04-04 13:44:14 -0300

    Implementando autenticação

--- js/script.js
+++ js/script.js
@@ -10,12 +10,100 @@ const nullMessage = document.getElementById('null');
 // Array para armazenar as tarefas
 let lista = [];
 
+// Verificar a autenticação ao carregar a página
+verificarAutenticacao();
+
 // Carregar os tarefas do Local Storage ao carregar a página
 loadLocalStorage();
 
 // Parâmetros da API
-const apiKey = 'a7c01d087b8508ae8cfb3b851fd8527107c8b878';
 const url = 'https://api.todoist.com/rest/v2/tasks';
+
+// Variáveis globais para armazenar o estado da autenticação
+let accessToken = null;
+let isAuthenticated = false;
+
+function solicitarAutorizacaoOAuth() {
+    const clientID = '7b81067ab59f4391a6113fb64c89ce28';
+    const scope1 = 'task:add';
+    const scope2 = 'data:delete';
+    const scope3 = 'data:read';
+    const scope4 = 'data:read_write';
+    const scope = `${scope1},${scope2},${scope3},${scope4}`; 
+    const state = '78678868685856';
+
+    const urlAutorizacao = `https://todoist.com/oauth/authorize?client_id=${clientID}&scope=${scope}&state=${state}`;
+
+    // Redirecionar o usuário para a URL de autorização
+    window.location.href = urlAutorizacao;
+}
+
+async function trocarCodigoPorAccessToken(codigo) {
+    const clientID = '7b81067ab59f4391a6113fb64c89ce28';
+    const clientSecret = '9c034aadafb140bf955fdeff8522fe37';
+    const redirectURI = 'https://matheusgodzilla.github.io/teste-to-do/';
+
+    const urlTrocaToken = 'https://todoist.com/oauth/access_token';
+
+    const parametros = {
+        client_id: clientID,
+        client_secret: clientSecret,
+        code: codigo,
+        redirect_uri: redirectURI
+    };
+
+    try {
+        const resposta = await fetch(urlTrocaToken, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(parametros)
+        });
+
+        if (!resposta.ok) {
+            throw new Error('Erro ao trocar código por token de acesso');
+        }
+
+        const dados = await resposta.json();
+        accessToken = dados.access_token;
+        isAuthenticated = true;
+
+        localStorage.setItem('accessToken', accessToken);
+
+        console.log('Token de acesso obtido:', accessToken);
+    } catch (erro) {
+        console.error('Ocorreu um erro ao trocar código por token de acesso:', erro);
+    }
+}
+
+function verificarAutenticacao() {
+    const urlAtual = new URL(window.location.href);
+    const codigo = urlAtual.searchParams.get('code');
+
+    if (codigo) {
+        trocarCodigoPorAccessToken(codigo);
+    } else {
+        alert('Usuário não autenticado. Redirecionando para a página de autenticação para logar no Todoist.');
+        solicitarAutorizacaoOAuth();
+    }
+}
+
+async function fazerChamadaAPI(url, options) {
+    if (!isAuthenticated) {
+        console.error('Usuário não autenticado');
+        return;
+    }
+
+    options.headers.Authorization = `Bearer ${accessToken}`;
+
+    try {
+        const resposta = await fetch(url, options);
+        return resposta.json();
+    } catch (erro) {
+        console.error('Ocorreu um erro ao fazer chamada à API:', erro);
+    }
+}
 
 function cadastrarTarefa(event) {
     event.preventDefault();
@@ -25,16 +113,14 @@ function cadastrarTarefa(event) {
     const categoria = categoriaInput.value;
     const data = dataInput.value;
   
-    const tarefa = { titulo, descricao, categoria, data, status: 'pendente'};
-  
-    lista.push(tarefa);
+    const tarefa = {titulo, descricao, categoria, data, status: 'pendente'};
 
     // Enviar tarefa para a API
     const options = {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
+          'Authorization': `Bearer ${accessToken}`
       },
       body: JSON.stringify({
           content: titulo,
@@ -55,6 +141,9 @@ function cadastrarTarefa(event) {
       .then(data => {
           console.log('Tarefa cadastrada:', data);
           tarefa.id = data.id;
+          lista.push(tarefa);
+          atualizarListaTarefas();
+          saveLocalStorage();
       })
       .catch(error => {
           console.error('Ocorreu um erro:', error);
@@ -63,10 +152,7 @@ function cadastrarTarefa(event) {
     // Limpar os campos de entrada
     tituloInput.value = '';
     descricaoInput.value = '';
-    dataInput.value = '';
-  
-    atualizarListaTarefas();
-    saveLocalStorage();
+    dataInput.value = '';   
 }
 
 function atualizarListaTarefas() {
@@ -148,7 +234,7 @@ function concluirTarefa(tarefa) {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
+          'Authorization': `Bearer ${accessToken}`
       },
       body: JSON.stringify({
           id: tarefaId
@@ -180,7 +266,7 @@ function excluirTarefa(tarefa) {
     const options = {
         method: 'DELETE',
         headers: {
-            'Authorization': `Bearer ${apiKey}`
+            'Authorization': `Bearer ${accessToken}`
         }
     };
   
